ddf8186985cf8478b9b3c4df6ada70d7cf87ffb0
AgustinAugurusa <agustin.augurusa@ing.edu.ar>
2020-03-16 17:26:28 -0300

    Test Insertion

--- src/test/tp1/ejercicio2/InsertionSortTest.java
+++ src/test/tp1/ejercicio2/InsertionSortTest.java
@@ -6,23 +6,30 @@ import static org.junit.Assert.*;
 public class InsertionSortTest {
 
     @Test
-    public void insertionSortTest() {
-
-    }
-
-    @Test
     public void intInsertionSortTest() {
-
+        InsertionSort insertionSort = new InsertionSort();
+        int[] a = {4,7,1,3,2,6,5};
+        int[] b = {1,2,3,4,5,6,7};
+        insertionSort.insertionSort(a);
+        assertArrayEquals(b,a);
     }
 
     @Test
     public void stringInsertionSortTest() {
-
+        InsertionSort insertionSort = new InsertionSort();
+        String[] a = {"Florencia", "Juan", "Agustin"};
+        String[] b = {"Agustin", "Florencia", "Juan"};
+        insertionSort.insertionSort(a);
+        assertArrayEquals(b,a);
     }
 
     @Test
     public void genericInsertionSortTest() {
-
+        InsertionSort insertionSort = new InsertionSort();
+        String[] a = {"b", "c", "a"};
+        String[] b = {"a", "b", "c"};
+        insertionSort.insertionSortGeneric(a);
+        assertArrayEquals(b,a);
     }
 
 
