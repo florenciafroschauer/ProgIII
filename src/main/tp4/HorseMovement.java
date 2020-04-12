34b190455a5b5675f2624b29cb52ce087b7d0cbc
florenciafroschauer <florencia.froschauer@ing.austral.edu.ar>
2020-04-12 20:30:55 -0300

    Horse Movement changes

--- src/main/tp4/HorseMovement.java
+++ src/main/tp4/HorseMovement.java
@@ -1,4 +1,13 @@
 package tp4;
 
+import exercise.DynamicStack;
+
 public class HorseMovement {
+
+    public DynamicStack<Integer> possibleMovement(Integer row, Integer column) {
+        DynamicStack<Integer> possibleMovement = new DynamicStack<>();
+
+
+        return possibleMovement;
+    }
 }
