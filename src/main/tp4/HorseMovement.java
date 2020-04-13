46ab69da597723cc705d70b28d57b9c6ed36a82a
AgustinAugurusa <agustin.augurusa@ing.edu.ar>
2020-04-12 22:40:55 -0300

    Cambios Caballo

--- src/main/tp4/HorseMovement.java
+++ src/main/tp4/HorseMovement.java
@@ -4,7 +4,7 @@ import exercise.DynamicStack;
 
 public class HorseMovement {
 
-    private DynamicStack<Coordinate> possibleMovement(Coordinate) {
+    private DynamicStack<Coordinate> possibleMovement(Coordinate coordinate) {
         DynamicStack<Coordinate>dynamicStack = new DynamicStack<>();
         return dynamicStack;
     }
