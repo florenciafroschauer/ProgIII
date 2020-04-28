fa585497cb54f55771f0cbd2dcdb2b4420f359fc
florenciafroschauer <florencia.froschauer@ing.austral.edu.ar>
2020-04-28 14:17:07 -0300

    BinarySearchTree

--- src/main/tp5/DoubleNode.java
+++ src/main/tp5/DoubleNode.java
@@ -5,15 +5,18 @@ package tp5;
  */
 
 public class DoubleNode<T> {
-    DoubleNode <T> right, left;
-    T data;
+    public DoubleNode <T> right, left;
+    public T data;
 
-    public DoubleNode(T o){
-        data = o;
+    public DoubleNode() {}
+
+    public DoubleNode(T data) {
+        this.data = data;
     }
-    public DoubleNode(T o, DoubleNode<T> left, DoubleNode<T> right) {
+
+    public DoubleNode(T data, DoubleNode<T> left, DoubleNode<T> right) {
         this.right = right;
         this.left = left;
-        data = o;
+        this.data = data;
     }
 }
---
+++ src/main/tp6/BinarySearchTree.java
@@ -0,0 +1,144 @@
+package tp6;
+
+import tp5.DoubleNode;
+
+public class BinarySearchTree<T> {
+    // Implementacion de un arbol binario de busqueda no balanceado
+    // Autor Alicia Gioia
+
+    private DoubleNode<T> root;
+
+    public BinarySearchTree() {
+        root = null;
+    }
+
+    // precondicion: elemento a insertar no pertenece al árbol
+    public void insert(Comparable <T> x) {
+        root = insert(root, x);
+    }
+
+    // precondicion: elemento a eliminar pertenece al árbol
+    public void delete(Comparable <T> x) {
+        root = delete(root, x);
+    }
+
+    // precondicion: árbol distinto de vacío
+    public T getMin() {
+        return getMin(root).data;
+    }
+
+    // precondicion: árbol distinto de vacío
+    public T getMax() {
+        return getMax(root).data;
+    }
+
+    // precondicion: elemnto a buscar pertenece al arbol
+    public T search(Comparable<T> x) {
+        return search(root, x).data;
+    }
+
+    // precondicion: -
+    public boolean exists(Comparable<T> x) {
+        return exists(root, x);
+    }
+
+    // precondicion: -
+    public boolean isEmpty() {
+        return (root == null);
+    }
+
+    // precondición: árbol distino de vacío
+    public T getRoot() {
+        return root.data;
+    }
+
+    // precondición: árbol distino de vacío
+    public BinarySearchTree<T> getLeft() {
+        BinarySearchTree<T> t = new BinarySearchTree<T>();
+        t.root = root.left;
+        return t;
+    }
+
+    // precondición: árbol distino de vacío
+    public BinarySearchTree<T> getRight() {
+        BinarySearchTree<T> t = new BinarySearchTree<T>();
+        t.root = root.right;
+        return t;
+    }
+
+
+    // METODOS PRIVADOS
+    private DoubleNode<T> getMax(DoubleNode <T> t) {
+        if (t.right == null)
+            return t;
+        else
+            return getMax(t.right);
+    }
+
+    private DoubleNode<T> getMin(DoubleNode <T> t) {
+        if (t.left == null)
+            return t;
+        else
+            return getMin(t.left);
+    }
+
+    private DoubleNode <T> search(DoubleNode <T> t, Comparable<T> x) {
+        if (x.compareTo(t.data) == 0)
+            return t;
+        else if (x.compareTo(t.data) < 0)
+            return search(t.left, x);
+        else
+            return search(t.right, x);
+    }
+
+    private boolean exists(DoubleNode <T> t, Comparable<T> x) {
+        if (t == null)
+            return false;
+        if (x.compareTo(t.data) == 0)
+            return true;
+        else if (x.compareTo(t.data) < 0)
+            return exists(t.left, x);
+        else
+            return exists(t.right, x);
+    }
+
+
+    private DoubleNode<T> insert(DoubleNode <T> t, Comparable <T> x) {
+        if (t == null) {
+            t = new DoubleNode<T>();
+            t.data = (T) x;
+        }
+
+        else if (x.compareTo(t.data) < 0)
+            t.left = insert(t.left, x);
+        else
+            t.right = insert(t.right, x);
+        return t;
+    }
+
+
+    private DoubleNode<T> delete(DoubleNode<T> t, Comparable<T> x) {
+        if (x.compareTo(t.data) < 0)
+            t.left = delete(t.left, x);
+        else if (x.compareTo(t.data) > 0)
+            t.right = delete(t.right, x);
+        else
+        if (t.left != null && t.right != null ) {
+            t.data = getMin(t.right).data;
+            t.right = deleteMin(t.right);
+        }
+        else if (t.left != null)
+            t = t.left;
+        else
+            t = t.right;
+        return t;
+    }
+
+    private DoubleNode<T> deleteMin(DoubleNode<T> t) {
+        if (t.left != null)
+            t.left = deleteMin(t.left);
+        else
+            t = t.right;
+        return t;
+    }
+}
